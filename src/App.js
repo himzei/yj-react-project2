aed2bd44c47cbe6fe4e5e80b34665b5e0271db4a
himzei <himzei@gmail.com>
2023-05-22 13:29:52 +0900

    13:30

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import {
   AiFillContacts,
 } from "react-icons/ai";
 
-const BOX_CSS = "";
-
 function App() {
   return (
     <>
@@ -28,138 +26,192 @@ function App() {
           </Box>
 
           {/* 본문 */}
-          <VStack w="inherit" py="140px" px={4} alignItems={"flex-start"}>
-            <Text fontWeight={600} fontSize={24}>
-              최신상품
-            </Text>
-            <Grid gap={2} gridTemplateColumns={"repeat(2, 1fr)"} w="full">
-              <GridItem
-                h="200px"
-                border={"1px"}
-                rounded={"md"}
-                borderColor="gray.300"
-                backgroundImage="url('https://images.unsplash.com/photo-1684516310710-463141e44242?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80')"
-                backgroundSize={"cover"}
-                backgroundPosition={"center center"}
-                position="relative"
-              >
-                <Box
-                  position="absolute"
-                  top={0}
-                  left={0}
-                  w="full"
-                  h="full"
-                  bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
+          <VStack
+            spacing={16}
+            w="inherit"
+            py="140px"
+            px={4}
+            alignItems={"flex-start"}
+          >
+            {/* 최신상품 컨테이너 */}
+            <VStack w="full" alignItems={"flex-start"}>
+              <Text fontWeight={600} fontSize={24}>
+                최신상품
+              </Text>
+              <Grid gap={2} gridTemplateColumns={"repeat(2, 1fr)"} w="full">
+                <GridItem
+                  h="200px"
+                  border={"1px"}
+                  rounded={"md"}
+                  borderColor="gray.300"
+                  backgroundImage="url('https://images.unsplash.com/photo-1684516310710-463141e44242?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80')"
+                  backgroundSize={"cover"}
+                  backgroundPosition={"center center"}
+                  position="relative"
                 >
-                  <Text
-                    color="white"
+                  <Box
                     position="absolute"
+                    top={0}
+                    left={0}
                     w="full"
-                    bottom="0"
-                    align="center"
-                    fontWeight={600}
-                    py={4}
+                    h="full"
+                    bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
                   >
-                    팔공산 투어
-                  </Text>
-                </Box>
-              </GridItem>
-              <GridItem
-                h="200px"
-                border={"1px"}
-                rounded={"md"}
-                borderColor="gray.300"
-                backgroundImage="url('https://plus.unsplash.com/premium_photo-1683299266036-9e9b6c9d9152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
-                backgroundSize={"cover"}
-                backgroundPosition={"center center"}
-                position="relative"
-              >
-                <Box
-                  position="absolute"
-                  top={0}
-                  left={0}
-                  w="full"
-                  h="full"
-                  bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
+                    <Text
+                      color="white"
+                      position="absolute"
+                      w="full"
+                      bottom="0"
+                      align="center"
+                      fontWeight={600}
+                      py={4}
+                    >
+                      팔공산 투어
+                    </Text>
+                  </Box>
+                </GridItem>
+                <GridItem
+                  h="200px"
+                  border={"1px"}
+                  rounded={"md"}
+                  borderColor="gray.300"
+                  backgroundImage="url('https://plus.unsplash.com/premium_photo-1683299266036-9e9b6c9d9152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
+                  backgroundSize={"cover"}
+                  backgroundPosition={"center center"}
+                  position="relative"
                 >
-                  <Text
-                    color="white"
+                  <Box
                     position="absolute"
+                    top={0}
+                    left={0}
                     w="full"
-                    bottom="0"
-                    align="center"
-                    fontWeight={600}
-                    py={4}
+                    h="full"
+                    bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
                   >
-                    지리산 투어
-                  </Text>
-                </Box>
-              </GridItem>
+                    <Text
+                      color="white"
+                      position="absolute"
+                      w="full"
+                      bottom="0"
+                      align="center"
+                      fontWeight={600}
+                      py={4}
+                    >
+                      지리산 투어
+                    </Text>
+                  </Box>
+                </GridItem>
 
-              <GridItem
-                h="200px"
-                border={"1px"}
-                rounded={"md"}
-                borderColor="gray.300"
-                backgroundImage="url('https://images.unsplash.com/photo-1682687220923-c58b9a4592ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
-                backgroundSize={"cover"}
-                backgroundPosition={"center center"}
-                position="relative"
-              >
-                <Box
-                  position="absolute"
-                  top={0}
-                  left={0}
-                  w="full"
-                  h="full"
-                  bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
+                <GridItem
+                  h="200px"
+                  border={"1px"}
+                  rounded={"md"}
+                  borderColor="gray.300"
+                  backgroundImage="url('https://images.unsplash.com/photo-1682687220923-c58b9a4592ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
+                  backgroundSize={"cover"}
+                  backgroundPosition={"center center"}
+                  position="relative"
                 >
-                  <Text
-                    color="white"
+                  <Box
                     position="absolute"
+                    top={0}
+                    left={0}
                     w="full"
-                    bottom="0"
-                    align="center"
-                    fontWeight={600}
-                    py={4}
+                    h="full"
+                    bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
                   >
-                    앞산 투어
-                  </Text>
-                </Box>
-              </GridItem>
+                    <Text
+                      color="white"
+                      position="absolute"
+                      w="full"
+                      bottom="0"
+                      align="center"
+                      fontWeight={600}
+                      py={4}
+                    >
+                      앞산 투어
+                    </Text>
+                  </Box>
+                </GridItem>
 
-              <GridItem
-                h="200px"
-                border={"1px"}
-                rounded={"md"}
-                borderColor="gray.300"
-                backgroundImage="url('https://images.unsplash.com/photo-1682686581220-689c34afb6ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
-                backgroundSize={"cover"}
-                backgroundPosition={"center center"}
-                position="relative"
-              >
-                <Box
-                  position="absolute"
-                  top={0}
-                  left={0}
-                  w="full"
-                  h="full"
-                  bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
+                <GridItem
+                  h="200px"
+                  border={"1px"}
+                  rounded={"md"}
+                  borderColor="gray.300"
+                  backgroundImage="url('https://images.unsplash.com/photo-1682686581220-689c34afb6ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
+                  backgroundSize={"cover"}
+                  backgroundPosition={"center center"}
+                  position="relative"
                 >
-                  <Text
-                    color="white"
+                  <Box
                     position="absolute"
+                    top={0}
+                    left={0}
                     w="full"
-                    bottom="0"
-                    align="center"
-                    fontWeight={600}
-                    py={4}
+                    h="full"
+                    bgGradient="linear(to-b, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.9))"
                   >
-                    뒷산 투어
-                  </Text>
-                </Box>
-              </GridItem>
-            </Grid>
+                    <Text
+                      color="white"
+                      position="absolute"
+                      w="full"
+                      bottom="0"
+                      align="center"
+                      fontWeight={600}
+                      py={4}
+                    >
+                      뒷산 투어
+                    </Text>
+                  </Box>
+                </GridItem>
+              </Grid>
+            </VStack>
+
+            {/* 이벤트 컨테이너 */}
+            <VStack w="full" alignItems={"flex-start"}>
+              <Text fontWeight={600} fontSize={24}>
+                이벤트
+              </Text>
+              <Grid w="full">
+                <GridItem
+                  rounded={"lg"}
+                  w="full"
+                  h="120px"
+                  backgroundImage={
+                    "url('https://images.unsplash.com/photo-1683380381470-8bb7e42aa5b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
+                  }
+                  backgroundSize="cover"
+                  backgroundPosition={"center center"}
+                  position={"relative"}
+                >
+                  <Box
+                    position="absolute"
+                    top={0}
+                    left={0}
+                    w="full"
+                    h="full"
+                    bgGradient="linear(to-b, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.7))"
+                  >
+                    <Text
+                      w="full"
+                      align="center"
+                      position="absolute"
+                      top="50%"
+                      left="50%"
+                      transform={"translate(-50%, -50%)"}
+                      fontWeight={900}
+                      fontSize="20"
+                      color="red.600"
+                    >
+                      6월 이벤트 10% 할인
+                    </Text>
+                  </Box>
+                </GridItem>
+                <GridItem>2</GridItem>
+                <GridItem>3</GridItem>
+              </Grid>
+            </VStack>
           </VStack>
 
           {/* tail */}
